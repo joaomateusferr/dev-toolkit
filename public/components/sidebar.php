bfb5d1e5985d592993166c8ac494004047b273bc
Joao Ferreira <joaomateusferr@gmail.com>
2022-06-08 19:40:11 -0300

    add sidebar

---
+++ public/components/sidebar.php
@@ -0,0 +1,4 @@
+<div class="sidebar">
+  	<a class="active" href="<?php echo $ProjectPath.'/?tab=HOME' ?>">Home</a>
+  	<a href="<?php echo $ProjectPath.'/?tab=CLIP_BOARD' ?>">Clip Board</a>
+</div>

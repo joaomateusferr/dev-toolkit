cc117e03ad4110cffafd9d266d7b3d2f2eaad499
Joao Ferreira <joaomateusferr@gmail.com>
2022-06-14 21:13:37 -0300

    add dummy file

--- public/components/sidebar.php
+++ public/components/sidebar.php
@@ -1,4 +1,5 @@
 <div class="sidebar">
   	<a <?php echo $CurrentTool == 'home' ? 'class="active"' : '';?> href="<?php echo "$ProjectPublicRoot/components/tools/home.php" ?>">Home</a>
   	<a <?php echo $CurrentTool == 'clip-board' ? 'class="active"' : '';?> href="<?php echo "$ProjectPublicRoot/components/tools/clip-board.php" ?>">Clip Board</a>
+	<a <?php echo $CurrentTool == 'dummy-file' ? 'class="active"' : '';?> href="<?php echo "$ProjectPublicRoot/components/tools/dummy-file.php" ?>">Dummy File</a>
 </div>
